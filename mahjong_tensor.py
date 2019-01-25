8506680ab42af5f9fed88b45a6b140ce90dd21e3
Richard Chang <DickChang88@gmail.com>
2019-01-25 15:04:34 -0800

    Add first draft model layers and prediction code
    does not work due to the layers expecting uniform sized inputs, while
    the actual inputs are variable sized

--- mahjong_tensor.py
+++ mahjong_tensor.py
@@ -5,20 +5,24 @@ import matplotlib.image as mpimg
 import random
 import math
 import numpy as np
+from tensorflow import keras
+import tensorflow as tf
 
 test_percent = 0.1 # Use this percent of our sample data for testing
 class_names = [] # this gets populated in load_data()
 
+# all images will be 3-channel, float colors in the range [0,1]
 def load_data():
     num_files_loaded = 0
     all_filenames = []
-    train_data = []
+    train_images = []
     train_labels = []
-    test_data = []
+    test_images = []
     test_labels = []
     data_dir = './images/mahjong/data_format/'
     all_filenames = os.listdir(data_dir)
 
+    # partition and count all the input files by type
     tile_accumulator = {}
     for file in all_filenames:
         file_split = file.split('_')
@@ -27,15 +31,23 @@ def load_data():
             tile_accumulator[tile_type] = 1
         else:
             tile_accumulator[tile_type] += 1
+
+    # create the class names
     tile_accumulator = OrderedDict(sorted(tile_accumulator.items()))
     class_names.extend(list(tile_accumulator.keys()))
 
+    # Load every file and divide them between the training set and the test set
+    # scale jpg images to the [0,1] range
     for tile_type in class_names:
         tile_type_imgs = []
         tile_type_filenames = [x for x in all_filenames if (tile_type+"_") in x]
         for filename in tile_type_filenames:
             num_files_loaded += 1
-            tile_type_imgs.append(mpimg.imread( data_dir + filename ))
+            loaded_image = mpimg.imread( data_dir + filename )
+            if '.png' in filename:
+                tile_type_imgs.append(loaded_image)
+            elif '.jpg' in filename:
+                tile_type_imgs.append(np.array(loaded_image, dtype="float") / 255.0 ) # scale jpeg images
             #print(filename + ": " + str(np.array(tile_type_imgs[-1]).shape))
 
         #print(tile_type_filenames)
@@ -43,34 +55,96 @@ def load_data():
 
         num_test_images = math.ceil(tile_accumulator[tile_type] * test_percent)
         for i in range(num_test_images):
-            test_data.append(tile_type_imgs.pop( random.randint(0, len(tile_type_imgs)-1) ))
+            test_images.append(tile_type_imgs.pop( random.randint(0, len(tile_type_imgs)-1) ))
             test_labels.append(class_names.index(tile_type))
 
-        train_data.extend(tile_type_imgs)
+        train_images.extend(tile_type_imgs)
         for i in range(len(tile_type_imgs)):
             train_labels.append(class_names.index(tile_type))
 
     print("Loaded " + str(num_files_loaded) + " files.")
-    print("Training data shape: " + str(np.array(train_data).shape))
+    print("Training images shape: " + str(np.array(train_images).shape))
     print("Training Labels shape: " + str(np.array(train_labels).shape))
-    print("Test Data shape: " + str(np.array(test_data).shape))
+    print("Test images shape: " + str(np.array(test_images).shape))
     print("Test Labels shape: " + str(np.array(test_labels).shape))
-    return (train_data, train_labels), (test_data, test_labels)
+    return (train_images, train_labels), (test_images, test_labels)
+
+def plot_single(image, filename):
+    plt.figure()
+    plt.imshow(image)
+    plt.colorbar()
+    plt.grid(False)
+    plt.savefig(filename)
+    plt.show()
 
 if __name__ == "__main__":       
-    (train_data, train_labels), (test_data, test_labels) = load_data()
-    print(class_names)
+    # import data set and define string labels
+    (train_images, train_labels), (test_images, test_labels) = load_data()
+    # print(class_names)
 
-#img = mpimg.imread('./images/mahjong/data_format/bamboo_1_0000019.jpg')
+    plot_single( train_images[20], "train_images_20.png" )
 
-#print(img)
-#print(img.shape)
+    # plot the first 25 training images
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        plt.subplot(5,5,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[train_labels[i]])
+    plt.savefig('train_images_first25.png')
+    plt.show()
 
-# plot the first training image and save to png
-#plt.figure()
-#plt.imshow(img)
-#plt.colorbar()
-#plt.grid(False)
-#plt.savefig('plot.png')
-#plt.show()
+    if keras.backend.image_data_format() == 'channels_first':
+        print("channels first")
+        my_shape = (3, None, None)
+    elif keras.backend.image_data_format() == 'channels_last':
+        print("channels last")
+        my_shape = (None, None, 3)
+    else:
+        print("Unknown keras.backend.image_data_format(): " + keras.backend.image_data_format())
+        exit(0)
 
+    # setup model layers
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=my_shape),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    # compile the model with loss function, optimizer, and metrics
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Feed the training data to the model - in this example, the train_images and train_labels arrays.
+    # The model learns to associate images and labels.
+    # We ask the model to make predictions about a test set - in this example, the test_images array
+    #   We verify that the predictions match the labels from the test_labels array.
+    model.fit(train_images, train_labels, epochs=5)
+
+    # Evaluate accuracy
+    # compare how the model performs on the test dataset
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test accuracy:', test_acc)
+
+    # make predictions
+    predictions = model.predict(test_images)
+
+    # look at 0th image and the prediction array
+    plot_image_and_values(0, predictions, test_labels, test_images)
+
+    # Plot the first X test images, their predicted label, and the true label
+    # Color correct predictions in blue, incorrect predictions in red
+    num_rows = 5
+    num_cols = 3
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions, test_labels, test_images)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions, test_labels)
+    plt.savefig('predictions_first15.png')
+    plt.show
